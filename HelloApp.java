829a96aeb76be200825146f10a96d19e6e6943cd
PranavKannegonda <pranav.kannegonda18@gmail.com>
2026-04-14 13:02:05 +0530

    Update HelloApp.java

--- HelloApp.java
+++ HelloApp.java
@@ -1,1 +1,12 @@
+#include <stdio.h>
 
+int main(int argc, char *argv[]) {
+    // Check if a name is provided as command-line argument
+    if (argc > 1) {
+        printf("Hello, %s\n", argv[1]);
+    } else {
+        // Default message if no argument is given
+        printf("Hello, World\n");
+    }
+    return 0;
+}
